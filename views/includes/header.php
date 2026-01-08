bdb1f165add2f8b59aa746c04201851ff5661e26
Dhika Primanda <dhikaprimanda.dp@gmail.com>
2026-01-08 21:16:11 +0700

    feat: add simple header

---
+++ views/includes/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+<body>
+    <header class="bg-gray-900 p-4 px-8 flex justify-between">
+        <h1 class="text-white">Task Manager</h1>
+        <h1 class="text-white font-bold">Login</h1>
+    </header>
+</body>
+</html>
