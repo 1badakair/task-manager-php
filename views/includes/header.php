41340ff0d2b5a064c8248c2160a7854334073cf5
Dhika Primanda <dhikaprimanda.dp@gmail.com>
2026-01-09 19:58:04 +0700

    style: edit header

--- views/includes/header.php
+++ views/includes/header.php
@@ -6,10 +6,8 @@
     <title>Document</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
-<body>
-    <header class="bg-gray-900 p-4 px-8 flex justify-between">
+<body class="bg-gray-600">
+    <header class="bg-gray-900 p-4 px-12 flex justify-between">
         <h1 class="text-white">Task Manager</h1>
         <h1 class="text-white font-bold">Login</h1>
     </header>
-</body>
-</html>
