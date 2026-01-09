3fbdc55f2ebb94158b1a3b687f3cf71b39929a26
Dhika Primanda <dhikaprimanda.dp@gmail.com>
2026-01-09 20:52:55 +0700

    feat: add page routing

--- index.php
+++ index.php
@@ -1,7 +1,33 @@
 <?php 
     require_once "app/config/database.php";
     require_once "app/models/User.php";
+    require_once "app/models/Task.php";
+    require_once "app/controllers/AuthController.php";
+    require_once "app/controllers/TaskController.php";
+
+    $action = isset($_GET['action']) ? $_GET['action'] : '';
 
     $user = new User($pdo);
-    header('Location: views/auth/login.php');
+    $task = new Task($pdo);
+
+    $userController = new AuthController($user);
+    $taskController = new TaskController($task);
+
+    switch($action){
+        case 'login':
+            $userController->login($_POST['username'], $_POST['password']);
+            break;
+        case 'register':
+            $userController->register($_POST['username'], $_POST['email'], $_POST['password']);
+            break;
+        case 'create':
+            $taskController->createTask($_POST['title'], $_POST['status'], $_POST['priority'], $_POST['due_date']);
+            break;
+        case 'edit':
+            $taskController->updateTask($_POST['id'], $_POST['title'], $_POST['status'], $_POST['priority'], $_POST['due_date']);
+            break;
+        case 'delete':
+            $taskController->deleteTask($_GET['id']);
+            break;
+    }
 ?>
